bfe05a729daa7640a54ab7a20830564db3962ebf
Egoist <epsilonelittr@gmail.com>
2022-07-30 20:12:55 +0300

    Update users table

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('name');
             $table->string('email')->unique();
-            //$table->string('username')->unique();
+            //$table->string('username')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
