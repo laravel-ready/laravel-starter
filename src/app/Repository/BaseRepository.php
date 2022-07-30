3436b7f0ccfa0b78d79227f35fd2ad7dae791964
Egoist <epsilonelittr@gmail.com>
2022-07-30 19:41:04 +0300

    Add repository pattern

---
+++ src/app/Repository/BaseRepository.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Container\Container as Application;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseRepository
+{
+    /**
+     * @var Model
+     */
+    protected Model $model;
+
+    /**
+     * @var Application
+     */
+    protected Application $app;
+
+    /**
+     * @param Application $app
+     *
+     * @throws \Exception
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+        $this->makeModel();
+    }
+
+    /**
+     * Get searchable fields array
+     *
+     * @return array
+     */
+    abstract public function getFieldsSearchable(): array;
+
+    /**
+     * Configure the Model
+     *
+     * @return string
+     */
+    abstract public function model(): string;
+
+    /**
+     * Make Model instance
+     *
+     * @return Model
+     * @throws \Exception
+     *
+     */
+    public function makeModel(): Model
+    {
+        $model = $this->app->make($this->model());
+
+        if (!$model instanceof Model) {
+            throw new \Exception("Class {$this->model()} must be an instance of Illuminate\\Database\\Eloquent\\Model");
+        }
+
+        return $this->model = $model;
+    }
+
+    /**
+     * Paginate records for scaffold.
+     *
+     * @param int $perPage
+     * @param array $columns
+     * @return LengthAwarePaginator
+     */
+    public function paginate(int $perPage, array $columns = ['*']): LengthAwarePaginator
+    {
+        $query = $this->allQuery();
+
+        return $query->paginate($perPage, $columns);
+    }
+
+    /**
+     * Build a query for retrieving all records.
+     *
+     * @param array $search
+     * @param int|null $skip
+     * @param int|null $limit
+     * @return Builder
+     */
+    public function allQuery(array $search = [], int $skip = null, int $limit = null): Builder
+    {
+        $query = $this->model->newQuery();
+
+        if (count($search)) {
+            foreach ($search as $key => $value) {
+                if (in_array($key, $this->getFieldsSearchable())) {
+                    $query->where($key, $value);
+                }
+            }
+        }
+
+        if (!is_null($skip)) {
+            $query->skip($skip);
+        }
+
+        if (!is_null($limit)) {
+            $query->limit($limit);
+        }
+
+        return $query;
+    }
+
+    /**
+     * Retrieve all records with given filter criteria
+     *
+     * @param array $search
+     * @param int|null $skip
+     * @param int|null $limit
+     * @param array $columns
+     *
+     * @return LengthAwarePaginator|Builder[]|Collection
+     */
+    public function all(array $search = [], int $skip = null, int $limit = null, array $columns = ['*']): Collection|LengthAwarePaginator|array
+    {
+        $query = $this->allQuery($search, $skip, $limit);
+
+        return $query->get($columns);
+    }
+
+    /**
+     * Create model record
+     *
+     * @param array $input
+     *
+     * @return Model
+     */
+    public function create(array $input): Model
+    {
+        $model = $this->model->newInstance($input);
+
+        $model->save();
+
+        return $model;
+    }
+
+    /**
+     * Find model record for given id
+     *
+     * @param int $id
+     * @param array $columns
+     *
+     * @return Builder|Builder[]|Collection|Model|null
+     */
+    public function find(int $id, array $columns = ['*']): Model|Collection|Builder|array|null
+    {
+        $query = $this->model->newQuery();
+
+        return $query->find($id, $columns);
+    }
+
+    /**
+     * Update model record for given id
+     *
+     * @param array $input
+     * @param int $id
+     *
+     * @return Builder|Builder[]|Collection|Model
+     */
+    public function update(array $input, int $id): Model|Collection|Builder|array
+    {
+        $query = $this->model->newQuery();
+
+        $model = $query->findOrFail($id);
+
+        $model->fill($input);
+
+        $model->save();
+
+        return $model;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return bool|mixed|null
+     * @throws \Exception
+     *
+     */
+    public function delete(int $id): mixed
+    {
+        $query = $this->model->newQuery();
+
+        $model = $query->findOrFail($id);
+
+        return $model->delete();
+    }
+}
