bae61a0147583e7d83fa72095f9bc84ecad47a8c
Egoist <epsilonelittr@gmail.com>
2022-07-30 15:38:36 +0300

    Improve default user seeder

--- src/database/seeders/User/DefaultUserSeeder.php
+++ src/database/seeders/User/DefaultUserSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders\User;
 
 use App\Models\User;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DefaultUserSeeder extends Seeder
 {
@@ -12,18 +14,33 @@ class DefaultUserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $defaultUser = 'admin@example.com';
+        $defaultUsers = [
+            [
+                'email' => 'super_admin@example.com',
+                'name' => 'Super Admin Madmin',
+                // 'username' => 'superAdmin',
+            ],
+            [
+                'email' => 'admin@example.com',
+                'name' => 'Admin Madmin',
+                // 'username' => 'admin',
+            ],
+            [
+                'email' => 'user@example.com',
+                'name' => 'User Muser',
+                // 'username' => 'userMuser',
+            ]
+        ];
 
-        if (User::count() === 0) {
-            $user = User::where(['email' => $defaultUser])->first();
-
-            if (! $user) {
-                User::factory()->count(1)->create([
-                    'name' => 'Administrator',
-                    'email' => $defaultUser,
-                    'email_verified_at' => now(),
+        foreach ($defaultUsers as $user) {
+            if (User::where('email', $user['email'])->count() === 0) {
+                User::create([
+                    'email' => $user['email'],
+                    'name' => $user['name'],
+                    // 'username' => $user['username'],
+                    'password' => Hash::make($user['email']),
                 ]);
             }
         }
