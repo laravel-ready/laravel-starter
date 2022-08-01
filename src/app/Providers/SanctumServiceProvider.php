34375728fee58ab3f8e2f49879d7a82131454bd4
Egoist <epsilonelittr@gmail.com>
2022-08-01 10:15:53 +0300

    Fix sanctum performance issue

---
+++ src/app/Providers/SanctumServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Laravel\Sanctum\PersonalAccessToken;
+
+class SanctumServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        PersonalAccessToken::updating($this->preventDatabaseWrite(...));
+    }
+
+    /**
+     * Sanctum updates a "last_used_at" column on every incoming request.
+     * This could possibly lead to deadlocks due to high concurrency.
+     * We don't need this information, so we're gonna ignore it.
+     */
+    private function preventDatabaseWrite(PersonalAccessToken $model)
+    {
+        return !$model->isDirty('last_used_at'); // returning "false" will cancel the "UPDATE" operation
+    }
+}
