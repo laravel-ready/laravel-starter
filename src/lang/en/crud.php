0fbe1e5ef8f2b6ea0a4f2cbdd7e4244af942b8d0
Egoist <epsilonelittr@gmail.com>
2022-08-05 12:22:37 +0300

    Add crud translations

---
+++ src/lang/en/crud.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'listed' => 'Listed',
+    'retrieved' => 'Retrieved',
+    'viewed' => 'Viewed',
+    'created' => 'Created',
+    'updated' => 'Updated',
+    'deleted' => 'Deleted',
+    'not_found' => 'Not found',
+    'could_not_deleted' => 'Could not deleted',
+    'could_not_updated' => 'Could not updated',
+    'exists' => 'Already Exists',
+    'invalid_file_name' => 'Invalid file name',
+    'error_while_uploading_file' => 'Error while uploading file',
+    'upload_success' => 'Upload success',
+
+];
---
+++ src/lang/tr/crud.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'listed' => 'Listelendi',
+    'retrieved' => 'Getirildi',
+    'viewed' => 'Görüntülendi',
+    'created' => 'Oluşturuldu',
+    'updated' => 'Güncellendi',
+    'deleted' => 'Silindi',
+    'not_found' => 'Bulunamadı',
+    'could_not_deleted' => 'Silinemedi',
+    'could_not_updated' => 'Güncellenemedi',
+    'exists' => 'Zaten Var',
+    'invalid_file_name' => 'Geçersiz dosya adı',
+    'error_while_uploading_file' => 'Dosya yüklenirken bir hata meydana geldi.',
+    'upload_success' => 'Yükleme başarılı',
+
+];
