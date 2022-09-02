dbdf54520215fcb5606f9349550fcfb9a70f97c9
Egoist <epsilonelittr@gmail.com>
2022-09-02 17:09:51 +0300

    Add storage link command

--- scripts/direct/install.bat
+++ scripts/direct/install.bat
@@ -11,6 +11,9 @@ CALL composer install
 @REM laravel app key
 CALL php artisan key:generate
 
+@REM link storage folder
+CALL php artisan storage:link
+
 @REM install NPM packges
 CALL npm i -s
 
--- scripts/direct/install.sh
+++ scripts/direct/install.sh
@@ -11,6 +11,9 @@ composer install
 # laravel app key
 php artisan key:generate
 
+# link storage folder
+php artisan storage:link
+
 # install NPM packges
 npm i -s
 
