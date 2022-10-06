0e5c56a13b936b8489141262c988543259f07d6d
Egoist <epsilonelittr@gmail.com>
2022-10-06 19:33:00 +0300

    Add auth middleware

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -22,7 +22,7 @@ return [
      * The middleware should enable session and cookies support in order for the Log Viewer to work.
      * The 'web' middleware will be applied automatically if empty.
      */
-    'middleware' => ['web'],
+    'middleware' => ['web', 'auth'],
 
     /**
      * Include file patterns
