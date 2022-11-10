b385e8933acf456f6f411b7c86e7c160825800de
Egoist <epsilonelittr@gmail.com>
2022-11-10 20:45:10 +0300

    Add custom auth template

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/apps/auth/auth.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
