1ae235d38cced9b8037fbe2ebccf39c009b63b95
Eyüp Erdoğan <epsilonelittr@gmail.com>
2023-05-27 05:49:35 +0300

    Update home blade

--- resources/views/web/pages/home/index.blade.php
+++ resources/views/web/pages/home/index.blade.php
@@ -1,23 +1,25 @@
 @extends('web.layouts.layout')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <section class="flex items-center justify-center min-h-screen py-24 mt-10 light:bg-white dark:bg-gray-900">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                        {{ __('You are logged in!') }}
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
