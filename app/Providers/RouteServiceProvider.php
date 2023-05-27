9e83bfaca8cd40833a2d00689972863c50af732a
Egoist <epsilonelittr@gmail.com>
2023-05-27 19:55:17 +0300

    Remove admin routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,10 +39,6 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
-
-            Route::middleware('web')
-                ->prefix('admin')
-                ->group(base_path('routes/admin.php'));
         });
     }
 
