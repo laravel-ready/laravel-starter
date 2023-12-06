230a6dd20cafc2a0025c03ae09b0f31b88b8b959
relliv <epsilonelittr@gmail.com>
2023-12-07 02:33:53 +0300

    Remove webpack mix

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,35 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/vendor/fortify-ui/sass/starter/auth.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
+	plugins: [
+		laravel({
+			input: ['resources/sass/app.scss', 'resources/vendor/fortify-ui/sass/starter/auth.scss', 'resources/js/app.js'],
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [tailwindcss],
+		},
+	},
+	server: {
+		port: 3000,
+		host: false,
+	},
 });
