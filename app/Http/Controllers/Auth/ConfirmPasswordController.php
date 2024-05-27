d81cb87a9ea665c4cbf6c8367592aaefd13097ec
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-05-27 17:11:16 +0300

    Remove unused laravel/ui trait

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\ConfirmsPasswords;
 
 class ConfirmPasswordController extends Controller
 {
@@ -18,8 +17,6 @@ class ConfirmPasswordController extends Controller
     | this trait and override any functions that require customization.
     |
     */
-
-    use ConfirmsPasswords;
 
     /**
      * Where to redirect users when the intended url fails.
