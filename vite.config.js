746dcee25ac427e2032d048d3ab42cc001f8a990
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-05-27 16:34:29 +0300

    Fix vitejs configs

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,23 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/vendor/fortify-ui/sass/starter/auth.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
+	plugins: [
+		laravel({
+			input: ['resources/sass/app.scss', 'resources/vendor/fortify-ui/sass/starter/auth.scss', 'resources/js/app.js'],
+			refresh: true,
+		}),
+		vue({
+			template: {
+				transformAssetUrls: {
+					base: null,
+					includeAbsolute: false,
+				},
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			vue: 'vue/dist/vue.esm-bundler.js',
+		},
+	},
 });
