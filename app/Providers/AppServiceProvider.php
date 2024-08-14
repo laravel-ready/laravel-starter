91b429a5ae4cc091bb7c78b5e3ccc86f6ac6ba35
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-08-14 13:56:27 +0300

    Fix missing IDE helper class on prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->isLocal()) {
+        if ($this->app->isLocal() && class_exists(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class)) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
     }
