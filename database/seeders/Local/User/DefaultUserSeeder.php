0dea5a5125273261fd70f15a361dd8b491d9deec
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-08-25 00:05:00 +0300

    Make default users variable public

--- database/seeders/Local/User/DefaultUserSeeder.php
+++ database/seeders/Local/User/DefaultUserSeeder.php
@@ -8,30 +8,30 @@ use Illuminate\Support\Facades\Hash;
 
 class DefaultUserSeeder extends Seeder
 {
+    public array $defaultUsers = [
+        [
+            'email' => 'super_admin@example.com',
+            'name' => 'Super Admin',
+            // 'username' => 'superAdmin',
+        ],
+        [
+            'email' => 'admin@example.com',
+            'name' => 'Admin',
+            // 'username' => 'admin',
+        ],
+        [
+            'email' => 'user@example.com',
+            'name' => 'User Muser',
+            // 'username' => 'userMuser',
+        ],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $defaultUsers = [
-            [
-                'email' => 'super_admin@example.com',
-                'name' => 'Super Admin Madmin',
-                // 'username' => 'superAdmin',
-            ],
-            [
-                'email' => 'admin@example.com',
-                'name' => 'Admin Madmin',
-                // 'username' => 'admin',
-            ],
-            [
-                'email' => 'user@example.com',
-                'name' => 'User Muser',
-                // 'username' => 'userMuser',
-            ],
-        ];
-
-        foreach ($defaultUsers as $user) {
+        foreach ($this->defaultUsers as $user) {
             if (User::where('email', $user['email'])->count() === 0) {
                 User::create([
                     'email' => $user['email'],
