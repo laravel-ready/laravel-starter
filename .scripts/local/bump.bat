3a83112826f3a6141b1fac892dfc3f4db38ae0ee
relliv <epsilonelittr@gmail.com>
2024-09-08 01:20:05 +0300

    Update package manager for windows

--- .scripts/local/bump.bat
+++ .scripts/local/bump.bat
@@ -4,7 +4,7 @@
 CALL pnpm add -g pnpm
 
 @REM check all updates and update them
-CALL npx --yes npm-check-updates --packageManager pnpm --target minor -u
+CALL npx --yes npm-check-updates --packageManager npm --target minor -u
 
 @REM install node dependencies
 CALL pnpm i --no-frozen-lockfile
