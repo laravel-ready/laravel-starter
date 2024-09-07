86a5d106b2d24569b1bf1f991cdc491d0aba5858
relliv <epsilonelittr@gmail.com>
2024-09-08 00:37:46 +0300

    Update bump scripts

---
+++ .scripts/local/bump.bat
@@ -0,0 +1,12 @@
+@REM update all packages to the latest minor and patch version
+
+CALL pnpm add -g pnpm
+
+@REM check all updates and update them
+CALL npx --yes npm-check-updates --packageManager pnpm --target minor -u
+
+@REM install node dependencies
+CALL pnpm i --no-frozen-lockfile
+
+@REM update composer dependencies
+CALL composer update
--- .scripts/local/bump.sh
+++ .scripts/local/bump.sh
@@ -1,5 +1,7 @@
 # update all packages to the latest minor and patch version
 
+pnpm add -g pnpm
+
 # check all updates and update them
 npx --yes npm-check-updates --packageManager pnpm --target minor -u
 
