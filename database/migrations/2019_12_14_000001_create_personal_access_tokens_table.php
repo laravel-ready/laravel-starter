dbd3e3d1370229c658a0343e545df2db9bae6523
Eyüp <epsilonelittr@gmail.com>
2025-05-06 17:33:18 +0300

    Add expires_at field to personal_access_tokens table

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamp('last_used_at')->nullable();
 
             $table->timestamps();
